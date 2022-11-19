c78dd965d7f7e5ddcbb8daf20451caceca60cf67
Mustafa BÜLBÜL <m.b-mustafa@outlook.com>
2022-11-19 23:20:53 +0300

    Menu is r!

--- Assets/Mustafas Folder/Script/TouchParticles.cs
+++ Assets/Mustafas Folder/Script/TouchParticles.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchParticles : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    public ParticleSystem yeap;
+    public float distFromCam = 10; //distance of the emitter from the camera
+    
+    private void Start()
     {
+        yeap.Pause();
+    }
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distFromCam));
+            yeap.transform.position = pos;
+            yeap.Play();
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            waiter();
+            yeap.Pause();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartCoroutine(Func<IEnumerator> wait)
     {
-        
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);    
     }
 }
+
